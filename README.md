1a70533643c2d4feec76d6c9a27054d00b1f2434
g-rocket <g.rocket1@gmail.com>
2014-01-14 13:00:50 -0500

    First Commit (setup repo)

---
+++ README.md
@@ -0,0 +1,17 @@
+#3D Drawing
+
+---
+###Description:
+It does stuff.  
+
+---
+###Goals:
+####Completed:
+* none so far
+
+
+####Short Term Plans:
+* make better readme
+
+####Long Term Plans:
+* make it work
---
+++ src/net/clonecomputers/lab/extra/draw3d/Draw3D.java
@@ -0,0 +1,12 @@
+package net.clonecomputers.lab.extra.draw3d;
+
+import javax.swing.JPanel;
+
+public class Draw3D extends JPanel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
