26b223ae8890dd054185dcb305ea2b1abfe201d2
g_rocket <g.rocket1@gmail.com>
2014-01-15 15:32:35 -0500

    add some basic code (still doesn't do anything)

--- src/net/clonecomputers/lab/extra/draw3d/Draw3D.java
+++ src/net/clonecomputers/lab/extra/draw3d/Draw3D.java
@@ -1,12 +1,38 @@
 package net.clonecomputers.lab.extra.draw3d;
 
-import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.image.*;
 
-public class Draw3D extends JPanel {
+import javax.swing.*;
+
+public class Draw3D extends JPanel implements Runnable {
+	private BufferedImage canvas;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int w = 600, h = 600;
+		if(args.length == 1) {
+			args = args[0].split(",");
+		}
+		if(args.length == 2) {
+			w = Integer.parseInt(args[0].trim());
+			h = Integer.parseInt(args[1].trim());
+		}
+		new Draw3D(w, h).run();
+	}
+	
 
+	public Draw3D(int width, int height) {
+		canvas = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+	}
+
+	@Override
+	public void run() {
+		//TODO: do stuff
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		g.drawImage(canvas, 0, 0, this);
 	}
 
 }
