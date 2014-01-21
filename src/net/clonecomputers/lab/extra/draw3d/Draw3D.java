7fca205192a636482e3e50c9f83592b0657771cf
g-rocket <g.rocket1@gmail.com>
2014-01-21 12:24:58 -0500

    add gui startup code

--- src/net/clonecomputers/lab/extra/draw3d/Draw3D.java
+++ src/net/clonecomputers/lab/extra/draw3d/Draw3D.java
@@ -1,12 +1,52 @@
 package net.clonecomputers.lab.extra.draw3d;
 
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class Draw3D extends JPanel {
+public class Draw3D extends JPanel implements Runnable {
+	private BufferedImage canvas;
+	
+	public Draw3D(Dimension dim) {
+		this.setPreferredSize(dim);
+		canvas = new BufferedImage(dim.width, dim.height, BufferedImage.TYPE_INT_RGB);
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		if(args.length == 1) args = args[0].split("[,]");
+		Dimension dim = new Dimension();
+		if(args.length >= 2) {
+			try{
+				dim.width = Integer.parseInt(args[0].trim());
+				dim.height = Integer.parseInt(args[1].trim());
+			} catch(NumberFormatException e) {
+				System.err.println("Invalid dimensions, using {600, 600}");
+				dim = new Dimension(600, 600);
+			}
+		} else {
+			dim = new Dimension(600, 600);
+		}
+		JFrame window = new JFrame("3D Drawing Program!");
+		Draw3D draw = new Draw3D(dim);
+		window.setContentPane(draw);
+		window.pack();
+		window.setVisible(true);
+		draw.run();
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		g.drawImage(canvas, 0, 0, this);
+	}
 
+	@Override
+	public void run() {
+		Graphics2D g = (Graphics2D)canvas.getGraphics();
+		//TODO: do stuff here
 	}
 
 }
