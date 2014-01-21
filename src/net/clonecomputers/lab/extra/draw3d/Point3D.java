4b8b91528ce75729bcb20dfe7563f9a81181d59e
g-rocket <g.rocket1@gmail.com>
2014-01-21 12:38:43 -0500

    add Point3D (direct copy from PointsOnSphere)

---
+++ src/net/clonecomputers/lab/extra/draw3d/Point3D.java
@@ -0,0 +1,59 @@
+package net.clonecomputers.lab.extra.draw3d;
+
+import static java.lang.Math.*;
+
+public class Point3D {
+	public final double x;
+	public final double y;
+	public final double z;
+	public Point3D(double x, double y, double z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	public String toString() {
+		return String.format("(%f,%f,%f)", x,y,z);
+	}
+	
+	public static double dot(Point3D a, Point3D b) {
+		return a.x*b.x + a.y*b.y + a.z*b.z;
+	}
+	public static Point3D cross(Point3D a, Point3D b) {
+		return new Point3D(
+			a.y*b.z - b.y*a.z,
+			a.z*b.x - b.z*a.x,
+			a.x*b.y - b.x*a.y
+		);
+	}
+	public static Point3D sum(Point3D a, Point3D b) {
+		return new Point3D(
+			a.x + b.x,
+			a.y + b.y,
+			a.z + b.z
+		);
+	}
+	public static Point3D sum(Point3D... points) {
+		Point3D sumSoFar = new Point3D(0,0,0);
+		for(Point3D p: points) {
+			sumSoFar = sum(sumSoFar,p);
+		}
+		return sumSoFar;
+	}
+	public static double abs(Point3D a) {
+		return sqrt(dot(a,a));
+	}
+	public static Point3D normalize(Point3D a) {
+		return product(a,1/abs(a));
+	}
+	public static Point3D product(Point3D a, double b) {
+		return new Point3D(
+			a.x * b,
+			a.y * b,
+			a.z * b
+		);
+	}
+	public static double dist(Point3D a, Point3D b) {
+		return abs(sum(a,product(b,-1)));
+	}
+	
+}
